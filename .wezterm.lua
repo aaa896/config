3257ef043cbc01e51705ffcc763597a17c873aba
aaa896 <177579786+aaa896@users.noreply.github.com>
2026-06-22 22:39:35 -0400

    Update .wezterm.lua

--- .wezterm.lua
+++ .wezterm.lua
@@ -1,358 +1,110 @@
-
+-- Pull in the wezterm API
 local wezterm = require 'wezterm'
+
 local act = wezterm.action
--- This table will hold the configuration.
-local config = {}
-local wezterm = require 'wezterm'
- default_prog = {"/bin/bash"}
+-- This will hold the configuration.
+local config = wezterm.config_builder()
+config.term = 'wezterm'
 
--- In newer versions of wezterm, use the config_builder which will
--- help provide clearer error messages
-if wezterm.config_builder then
-    config = wezterm.config_builder()
-end
+config.enable_wayland = false
+
+
+-- This is where you actually apply your config choices.
+
+-- For example, changing the initial geometry for new windows:
+config.initial_cols = 120
+config.initial_rows = 28
+
+-- or, changing the font size and color scheme.
+config.colors = {
+  -- The default text color
+  foreground = '#111111',
+  -- The default background color
+  background = '#f2f2f2',
+
+  -- Overrides the cell background color when the current cell is occupied by the
+  -- cursor and the cursor style is set to Block
+
+    cursor_bg = "#a99999",
+  -- Overrides the text color when the current cell is occupied by the cursor
+  cursor_fg = 'black',
+    split = '#1c150b',
+
+  tab_bar = {
+    -- The color of the strip that goes along the top of the window
+    -- (does not apply when fancy tab bar is in use)
+    background = '#f2f2f2',
+
+    -- The active tab is the one that has focus in the window
+    active_tab = {
+      -- The color of the background area for the tab
+      bg_color = '#a1c4d0',
+      -- The color of the text for the tab
+      fg_color = '#444444',
+
+      -- Specify whether you want "Half", "Normal" or "Bold" intensity for the
+      -- label shown for this tab.
+      -- The default is "Normal"
+      intensity = 'Normal',
+
+      -- Specify whether you want "None", "Single" or "Double" underline for
+      -- label shown for this tab.
+      -- The default is "None"
+      underline = 'None',
+
+      -- Specify whether you want the text to be italic (true) or not (false)
+      -- for this tab.  The default is false.
+      italic = false,
+
+      -- Specify whether you want the text to be rendered with strikethrough (true)
+      -- or not for this tab.  The default is false.
+      strikethrough = false,
+    },
+
+        new_tab = {
+      bg_color = '#f2f2f2',
+      fg_color = '#444444',
+
+      -- The same options that were listed under the `active_tab` section above
+      -- can also be used for `new_tab`.
+    },
+
+    -- Inactive tabs are the tabs that do not have focus
+    inactive_tab = {
+      bg_color = '#f2f2f2',
+      fg_color = '#444444',
+
+      -- The same options that were listed under the `active_tab` section above
+      -- can also be used for `inactive_tab`.
+    },
+},
+
+
+  }
 config.inactive_pane_hsb = {
     saturation = 1,
     brightness = 1
 }
 
-
--- Custom event to handle splitting with Neovim's working directory
-wezterm.on('split_with_nvim_cwd', function(window, pane)
-    -- Perform an action to get the working directory from Neovim
-    window:perform_action(act.SendString('echo getcwd()\n'), pane)
-
-    -- Split horizontally using the obtained cwd (simply adding the action for split)
-    window:perform_action(act.SplitHorizontal { cwd = pane:get_current_working_dir() }, pane)
-end)
-
-
-config.window_background_opacity =1
-  config.text_background_opacity = 1.0
---config.enable_tab_bar = false;
-config.enable_wayland = true;
---
--- This is where you actually apply your config choices
-config.use_fancy_tab_bar=false
-config.tab_bar_at_bottom = true
----- For example, changing the color scheme:
----
-config.color_scheme = 'Edge Dark (base16)'
-
-config.warn_about_missing_glyphs=false
-config.font = wezterm.font_with_fallback {
-    'Liberation Mono',
-    'Terminus',
-    'JetBrainsMono',
-     'Noto Color Emoji',
-}
-
-config.harfbuzz_features = { 'calt=0', 'clig=0', 'liga=0' }
-config.font_size=10.5
-config.tab_max_width = 24
-
-
-
-
-config.show_new_tab_button_in_tab_bar = false
-
-
-config.window_frame = {
-    font_size = 10,
-
-}
-
-
-config.colors = {
-    split = '#1c150b',
-
-    cursor_bg = "#c18c8e",
---#795151
---#945E57
-    --dark #3B2623
-    --l2 background= "#82524B",
-    --- red background= "#5E3030",
-    ---#6C4642
-    ---8a5a50
-    ---54393a
-    --454d32
-    --52593b
-    --light g
-    --524b2a
-    --#4a412b
-    --#49402d
-    --#331e05
-    --211103
-    --231705
-    --#3b2c20
-    --2c1608
-    --#211208
-    --351f19
-    --#4e3018
-    --#493822
-    --#433828
-    --#433b28
-    --#443827
-    --#4a3d2b
-    --#433a28
-    --3e3526
-    --#3d3424
-    --301e0a
-   -- #2d1e09
-   -- 351f19
-   -- #403726
-   -- 3d3424
-   -- 393121
-   --2b190d
-   --#443a27
-   --#413725
-   --2c271c
-   --#121212
-    background= "#f7f7f7",
-    --#7d453e
-    -- #855a52
-
-    foreground = '#444444',
-
-
-
-    tab_bar = {
-        -- The color of the strip that goes along the top of the window
-        -- (does not apply when fancy tab bar is in use)
-        --
-        background="1c150b00",
-        active_tab = {
-            -- The color of the background area for the tab
-            bg_color = "#a1c4d0",
-            -- The color of the text for the tab
-            fg_color = '#444444',
-        },
-        inactive_tab = {
-            bg_color = "1c150b00",
-            fg_color = '444444',
-
-            -- The same options that were listed under the `active_tab` section above
-            -- can also be used for `inactive_tab`.
-        },
-
-
-
-    },
-
-
-
-}
-
-
---end colors
+config.font_size = 10
+config.color_scheme = 'Papercolor Light (Gogh)'
+config.tab_bar_at_bottom  = true
+config.use_fancy_tab_bar  = false
+config.font = wezterm.font 'Liberation Mono'
 config.keys = {
-    -- activate pane selection mode with the default alphabet (labels are "a", "s", "d", "f" and so on)
-    {
-        key = '2', mods = 'CTRL',
-        action = act.PaneSelect {
-            mode="MoveToNewTab",
-        },
-    },
-    -- activate pane selection mode with numeric labels
-    {
-        key = '1',
-        mods = 'CTRL',
-        action = act.PaneSelect {
-            --alphabet = '1234567890',
-            mode = 'MoveToNewWindow',
-        },
-    },
-    -- show the pane selection mode, but have it swap the active and selected panes
-    {
-        key = '3',
-        mods = 'CTRL',
-        action = act.PaneSelect {
-            mode="Activate",
-        },
-    },
-    {
-        key = '0',
-        mods = 'CTRL',
-        action = act.PaneSelect {
-            mode = 'SwapWithActiveKeepFocus',
-        },
-    },
-    {
+  { key = 'w', mods = 'ALT', action = wezterm.action.CloseCurrentPane { confirm = true }, },
 
-        key = 'w',
-
-        mods = 'CTRL|SHIFT',
-
-        action = wezterm.action.CloseCurrentPane { confirm = true },
-
-    },
-
-    {
-        key = 'F5',
-        action = wezterm.action_callback(function(window, pane)
-            -- Read the path from /tmp/nvim_cwd
-            local f = io.open("/tmp/nvim_cwd", "r")
-            if f then
-                local nvim_cwd = f:read("*all"):gsub("\n", "") -- Read and strip the newline
-                f:close()
-
-                -- Spawn a new window using the read path as the working directory
-                window:perform_action(wezterm.action.SpawnCommandInNewWindow {
-                    cwd = nvim_cwd,  -- Set the working directory
-                    args = { './r.sh' },  -- Run the script
-                }, pane)
-            else
-                -- Handle the case where the file doesn't exist or can't be read
-                window:toast_notification("Error", "Could not read /tmp/nvim_cwd", nil, 5000)
-            end
-        end),
-    },
-
-    {
-      key = 'h',
-      mods = 'CTRL|SHIFT',
-      action = wezterm.action.ActivatePaneDirection('Left'),
-    },
-    {
-      key = 'j',
-      mods = 'CTRL|SHIFT',
-      action = wezterm.action.ActivatePaneDirection('Down'),
-    },
-    {
-      key = 'k',
-      mods = 'CTRL|SHIFT',
-      action = wezterm.action.ActivatePaneDirection('Up'),
-    },
-    {
-      key = 'l',
-      mods = 'CTRL|SHIFT',
-      action = wezterm.action.ActivatePaneDirection('Right'),
-    },
-
-    {
-        key = 'm',
-        mods = 'ALT',
-        action = wezterm.action_callback(function(window, pane)
-            -- Read the path from /tmp/nvim_cwd
-            local f = io.open("/tmp/nvim_cwd", "r")
-            if f then
-                local nvim_cwd = f:read("*all"):gsub("\n", "") -- Read and strip the newline
-                f:close()
-
-                -- Spawn a new window using the read path as the working directory
-                window:perform_action(wezterm.action.SpawnCommandInNewWindow {
-                    cwd = nvim_cwd,  -- Set the working directory
-                    args = { './b.sh' },  -- Run the script
-                }, pane)
-            else
-                -- Handle the case where the file doesn't exist or can't be read
-                window:toast_notification("Error", "m alt Could not read /tmp/nvim_cwd", nil, 5000)
-            end
-        end),
-    },
-
-    {
-        key = 'm',
-        mods = 'CTRL',
-        action = wezterm.action.SpawnCommandInNewWindow {
-            args = { 
-                --          '/bin/bash',        -- The shell program
-                --         '-c',               -- Option to run a command string
-                './b.sh', -- exec /bin/bash'  -- Command string: run the script and start a new shell
-            },
-        },
-    },
-
-    {
-        key = 'F5',
-        mods = 'CTRL',
-        action = wezterm.action.SpawnCommandInNewWindow {
-            args = { 
-                --          '/bin/bash',        -- The shell program
-                --         '-c',               -- Option to run a command string
-                './r.sh', -- exec /bin/bash'  -- Command string: run the script and start a new shell
-            },
-        },
-    },
-
-    {
-        key = '%',
-        mods = 'CTRL|ALT',
-        mods = 'CTRL|SHIFT',
-        action = wezterm.action_callback(function(window, pane)
-            -- Read the path from /tmp/nvim_cwd
-            local f = io.open("/tmp/nvim_cwd", "r")
-            if f then
-                local nvim_cwd = f:read("*all"):gsub("\n", "") -- Read and strip newline
-                f:close()
-
-                -- Split pane vertically using the read path as the working directory
-                -- Split pane horizontally using the read path as the working directory
-                window:perform_action(wezterm.action.SplitPane {
-                    direction = "Right",  -- Vertical split
-                    direction = "Right",  -- Horizontal split
-                    command = {
-                        args = { '/bin/bash' },  -- Open the shell in the new pane
-                        cwd = nvim_cwd,  -- Set the working directory
-                    },
-                    top_level = false, -- Create a new top-level window for the command
-                }, pane)
-            else
-                window:toast_notification("Error", "Could not read /tmp/nvim_cwd", nil, 5000)
-            end
-        end),
-    },
-
-    {
-        key = '"',
-        mods = 'CTRL|SHIFT',
-        action = wezterm.action_callback(function(window, pane)
-            -- Read the path from /tmp/nvim_cwd
-            local f = io.open("/tmp/nvim_cwd", "r")
-            if f then
-                local nvim_cwd = f:read("*all"):gsub("\n", "") -- Read and strip newline
-                f:close()
-
-                -- Split pane horizontally using the read path as the working directory
-                window:perform_action(wezterm.action.SplitPane {
-                    direction = "Down",  -- Horizontal split
-                    command = {
-                        args = { '/bin/bash' },  -- Open the shell in the new pane
-                        cwd = nvim_cwd,  -- Set the working directory
-                    },
-                    top_level = false, -- Create a new top-level window for the command
-                }, pane)
-            else
-                window:toast_notification("Error", "Could not read /tmp/nvim_cwd", nil, 5000)
-            end
-        end),
-    },
-
-
-   -- Move tab left
-    {
-      key = "PageUp",
-      mods = "CTRL|SHIFT",
-      action = wezterm.action.MoveTabRelative(-1),
-    },
-    -- Move tab right
-    {
-      key = "PageDown",
-      mods = "CTRL|SHIFT",
-      action = wezterm.action.MoveTabRelative(1),
-    },
-
-
-  { key = 'F9', mods = 'ALT', action = wezterm.action.ShowTabNavigator },
-
+  { key = '{', mods = 'SHIFT|ALT', action = act.MoveTabRelative(-1) },
+  { key = '}', mods = 'SHIFT|ALT', action = act.MoveTabRelative(1) },
+  { key = '@', mods = 'SHIFT|ALT', action = act.PaneSelect{mode ='MoveToNewTab',},},
+  { key = '!', mods = 'SHIFT|ALT', action = act.PaneSelect{mode ='MoveToNewWindow',},},
+  { key = 'h', mods = 'CTRL|SHIFT', action = wezterm.action.ActivatePaneDirection('Left'), },
+  { key = 'j', mods = 'CTRL|SHIFT', action = wezterm.action.ActivatePaneDirection('Down'), },
+  { key = 'k', mods = 'CTRL|SHIFT', action = wezterm.action.ActivatePaneDirection('Up'), }, 
+  { key = 'l', mods = 'CTRL|SHIFT', action = wezterm.action.ActivatePaneDirection('Right'), },
+  { key = 'H', mods = 'SHIFT|CTRL', action = act.Search { CaseInSensitiveString = 'hash' }, },
 
 }
+-- Finally, return the configuration to wezterm:
 
--- and finally, return the configuration to wezterm
 return config
-
-
-
-
-
-
